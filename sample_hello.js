a308979e6a8207a248469cff8bffe2de6da86466
im <poson_hemp@yahoo.co.jp>
2020-04-08 22:41:52 +0900

    update: multi pages

--- sample_hello.js
+++ sample_hello.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello, Express test!</h1>");
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Hello, Express test!</h1>" +
+      "<p><a href='/test1'>Test 1</a></p>" +
+      "<p><a href='/test2'>Test 2</a></p>"
+  );
+});
+
+app.get("/test1", (req, res) => {
+  res.send("<h1>This page is Test 1.</h1>" + "<p><a href='/'>Home</a></p>");
+});
+
+app.get("/test2", (req, res) => {
+  res.send("<h1>This page is Test 2.</h1>" + "<p><a href='/'>Home</a></p>");
 });
 
 app.listen(port, () => {
