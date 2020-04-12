a1f04c4ffe724753142b3aca8af773cfde172522
im <poson_hemp@yahoo.co.jp>
2020-04-12 18:18:00 +0900

    add function: add list item

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ $ npm init
 $ npm install --save express
 $ node xxxxxx.js
 ```
+
+```
+$ npm install --save multer
+$ npm install --save uuid
+```
--- api_list.js
+++ api_list.js
@@ -1,21 +1,41 @@
 const express = require("express");
+const multer = require("multer");
+const uuidv4 = require("uuid/v4");
+
 const app = express();
 const port = 3000;
 
+app.use(multer().none());
 app.use(express.static("web"));
+
+// ToDoList data
+const todoList = [];
 
 // API page
 // http://locaslhost:3000/api/v1/list
 app.get("/api/v1/list", (req, res) => {
-  // JSON data
-  const todoList = [
-    { title: "Node.js confirm.", done: true },
-    { title: "Express confirm.", done: false },
-    { title: "JavaScript confirm.", done: false },
-  ];
-
   // submit JSON
   res.json(todoList);
+});
+
+// add item to ToDoList
+// http://locaslhost:3000/api/v1/add
+app.post("/api/v1/add", (req, res) => {
+  const id = uuidv4;
+  const todoData = req.body;
+  const todoTitle = todoData.title;
+
+  const todoItem = {
+    id,
+    title: todoTitle,
+    done: false,
+  };
+
+  todoList.push(todoItem);
+
+  console.log("Add: " + JSON.stringify(todoItem));
+
+  res.json(todoItem);
 });
 
 app.listen(port, () => {
