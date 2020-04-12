7823c26f19d3d1ac40e99ae0d693d7dcde1eab68
im <poson_hemp@yahoo.co.jp>
2020-04-12 20:37:15 +0900

    add comments

--- api_list.js
+++ api_list.js
@@ -11,33 +11,39 @@ app.use(express.static("web"));
 // ToDoList data
 const todoList = [];
 
-// API page
+// API page (submit JSON)
 // http://locaslhost:3000/api/v1/list
 app.get("/api/v1/list", (req, res) => {
-  // submit JSON
   res.json(todoList);
 });
 
 // add item to ToDoList
 // http://locaslhost:3000/api/v1/add
 app.post("/api/v1/add", (req, res) => {
+  // generate uniq ID
   const id = uuidv4;
+
+  // fetch submit data from client
   const todoData = req.body;
   const todoTitle = todoData.title;
 
+  // generate ToDo set
   const todoItem = {
     id,
     title: todoTitle,
     done: false,
   };
 
+  // add item to ToDoList
   todoList.push(todoItem);
 
   console.log("Add: " + JSON.stringify(todoItem));
 
+  // response added item to client
   res.json(todoItem);
 });
 
+// set up server
 app.listen(port, () => {
   console.log("API Server running", `http://localhost:${port}/api/v1/list`);
 });
